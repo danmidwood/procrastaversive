c0073e266f816763b102399b8c443c95e6fbed6f
Dan Midwood <dan@danmidwood.com>
2014-07-19 12:29:41 +0200

    lein-release plugin: bumped version from 0.1.0 to 0.1.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject procrastiversives "0.1.0"
+(defproject procrastiversives "0.1.1-SNAPSHOT"
   :description "Punishing procrastination"
   :url "http://procrastiversives"
   :license {:name "Eclipse Public License"
